a075a35c123609193db291fca5da055b5f7e151b
Jxzx <2729239741@qq.com>
2023-03-14 10:24:10 +0800

    feat(‘添加layout’)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <n-config-provider :theme="theme">
     <router-view></router-view>
-    <n-card>
-      <n-space>
-        <n-button @click="theme = darkTheme">深色</n-button>
-        <n-button @click="theme = null">浅色</n-button>
-      </n-space>
-    </n-card>
   </n-config-provider>
 </template>
 
